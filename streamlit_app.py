ebbb8d22886d0c1869f7a61106b4055912ec0c9f
Vincent Liao <vincentliao@gmail.com>
2023-04-17 17:44:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,4 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
 for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+    st.write(f"{row.date} {row.tag} {row.content}")
